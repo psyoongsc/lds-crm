1f1ae58eeaf840f3b7866d6094d0c464fe83af7a
Park SeongYong <sh__y_@naver.com>
2022-03-31 17:31:57 +0900

    item manage main form complete

--- routes/item.js
+++ routes/item.js
@@ -4,7 +4,25 @@ var router = express.Router();
 const getConnection = require('../config/database');
 
 router.get('/', function(req, res, next) {
+    var page = Number(req.query.page) || 1;
+    var perPage = Number(req.query.perPage) || 10;
 
+    getConnection((conn => {
+        var sql = 'SELECT code, itemName, price FROM Item LIMIT ?, ?;'
+        var params = [page, perPage];
+
+        conn.query(sql, params, function(err, rows, fields) {
+            if(err) {
+                console.log('[ERROR] selecting item list has problem\n' + err);
+                res.render('error');
+            }
+            else {
+                res.render('layout/item_manage/main', {page: page, perPage: perPage, items: rows})
+            }
+        })
+
+        conn.release();
+    }))
 })
 
 router.get('/create', function(req, res, next) {
