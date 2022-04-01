b62e424b48d563ac57d88dd8b212b6157177ea75
Park SeongYong <sh__y_@naver.com>
2022-04-01 13:18:04 +0900

    220401 item manage backup

--- routes/item.js
+++ routes/item.js
@@ -6,10 +6,15 @@ const getConnection = require('../config/database');
 router.get('/', function(req, res, next) {
     var page = Number(req.query.page) || 1;
     var perPage = Number(req.query.perPage) || 10;
+    var search = '%'+ (req.query.search || '') +'%';
 
     getConnection((conn => {
-        var sql = 'SELECT code, itemName, price FROM Item LIMIT ?, ?;'
-        var params = [page, perPage];
+        var sql = 'SELECT code, itemName, CONCAT(FORMAT(price, 0), "원") as price FROM Item '
+        sql = sql + 'WHERE LOWER(code) LIKE LOWER(?) OR  REPLACE(LOWER(itemName), " ", "") LIKE REPLACE(LOWER(?), " ", "") '
+        sql = sql + 'LIMIT ?, ?;'
+        sql = sql + 'SELECT count(*) as count FROM Item '
+        sql = sql + 'WHERE LOWER(code) LIKE LOWER(?) OR  REPLACE(LOWER(itemName), " ", "") LIKE REPLACE(LOWER(?), " ", "") ORDER BY code;'
+        var params = [search, search, perPage*(page-1), perPage, search, search];
 
         conn.query(sql, params, function(err, rows, fields) {
             if(err) {
@@ -17,7 +22,7 @@ router.get('/', function(req, res, next) {
                 res.render('error');
             }
             else {
-                res.render('layout/item_manage/main', {page: page, perPage: perPage, items: rows})
+                res.render('layout/item_manage/main', {page: page, perPage: perPage, search: req.query.search || '', items: rows[0], counts: rows[1][0].count})
             }
         })
 
